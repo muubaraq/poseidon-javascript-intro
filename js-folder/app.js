304f48d404d14404cb6aac02dc91bdf0eeadbfa5
muubaraq <sixthlogic@gmail.com>
2022-05-28 18:53:26 +0100

    quad function

--- js-folder/app.js
+++ js-folder/app.js
@@ -64,23 +64,27 @@ console.log(`A ${adjective} ${noun} that ${verb}s fast`);
 Write a function that will print out the remainder of a division. This function will take two parameters 
  */
 
-function poseidonFunction(a, b){
-    return a % b;
+
+const poseidonFunction = (a, b) => { 
+  return a % b;
 }
+
 console.log(poseidonFunction(10, 3));
-
-
 
 /*
 Write a function that will solve quadratic equations using the almighty formula. This will take three parameters 
   */
 
-function quadFunction (a, b, c){
-    var result = ((b * b) - Math.sqrt(4 * a * c)) / (2 * a);
-    var result2 = ((b * b) + Math.sqrt(4 * a * c)) / (2 * a);
-    return result + " and " + result2
+
+
+const quadFunction = (a, b, c) => {
+  let root = Math.pow(b, 2) - (4 * a * c);
+  let x1 = (-1 * b + Math.sqrt(root)) / 2 * a;
+  let x2 = (-1 * b - Math.sqrt(root)) / 2 * a;
+  return x1 + " and " + x2;
 }
-console.log(quadFunction(1, 2, 1));
+
+console.log(quadFunction(1, 2, -1));
 
 
 
